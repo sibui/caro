bb37baf33191d94239e545866135aeec9186b00c
cse135 <cse135@ucsd.edu>
2015-05-05 10:55:25 -0700

    updated tables with references and added purchases table

--- students-with-database/ddl.sql
+++ students-with-database/ddl.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE students (
+CREATE TABLE students (
     id          SERIAL PRIMARY KEY,
     pid         INTEGER,
     first_name  TEXT,
@@ -33,11 +33,11 @@ INSERT INTO categories(description, name, numProducts) values ('This is my test 
 CREATE TABLE products (
 name TEXT NOT NULL CHECK (char_length(name) > 0),
 sku TEXT UNIQUE NOT NULL CHECK (char_length(sku) > 0),
-category TEXT NOT NULL CHECK (char_length(category) > 0),
-price INTEGER
+category TEXT NOT NULL CHECK (char_length(category) > 0) references categories(name),
+price INTEGER CHECK (price > 0)
 );
 
-INSERT INTO products(name, sku, category, price) values ('testname', 'test008', 'tacos', 0);
+INSERT INTO products(name, sku, category, price) values ('testname', 'test008', 'test', 5);
 
 CREATE TABLE carts (
 owner TEXT NOT NULL CHECK (char_length(owner) > 0) references users(name),
@@ -46,4 +46,14 @@ quantity INTEGER,
 price INTEGER
 );
 
-INSERT INTO carts(owner, sku, quantity, price) values ('simon', 'test008', '1', 0);
+INSERT INTO carts(owner, sku, quantity, price) values ('simon', 'test008', 1, 5);
+
+CREATE TABLE purchases (
+ts timestamp,
+customer TEXT NOT NULL CHECK (char_length(customer) > 0) references users(name),
+sku TEXT NOT NULL CHECK (char_length(sku) > 0) references products(sku),
+price INTEGER,
+quantity INTEGER
+);
+
+INSERT INTO purchases(ts, customer, sku, price, quantity) values (CURRENT_TIMESTAMP, 'simon','test008', 1, 5);
